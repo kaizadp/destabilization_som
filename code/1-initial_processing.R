b51919a54a978f3a80af6e277afa6c45f2da4df6
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-09-03 13:47:17 -0400

    import respiration google sheet and save

--- code/1-initial_processing.R
+++ code/1-initial_processing.R
@@ -5,7 +5,8 @@ library(googlesheets4)
 
 
 # load files from googlesheets -----------------------------------------
-core_key = read_sheet("1k7-Xdav-tRB13cyf3As_MOUk339u3XUuUrPmRwOg7AU")
+core_key = read_sheet("1k7-Xdav-tRB13cyf3As_MOUk339u3XUuUrPmRwOg7AU") %>% 
+  write.csv("data/core_key.csv", row.names = F)
 
 core_weights = read_sheet("1PR-VvyKcZIYoH3VF8bBWmzkmAnPibf0Fyd_EVrEqvcc")
 
@@ -16,3 +17,7 @@ core_weights_drydown = read_sheet("1PR-VvyKcZIYoH3VF8bBWmzkmAnPibf0Fyd_EVrEqvcc"
                                   sheet = "drydown")
 
 
+
+# respiration -------------------------------------------------------------
+read_sheet("1GWK1c5ruKiZmJ6mjZzCk-gBw6dL0eJ1OxT0OKHBsVbo") %>% 
+  write.csv("data/respiration.csv", row.names = F)
