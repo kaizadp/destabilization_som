750b5978c73bb76ce72afba6865353395dc1e900
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-09-24 11:24:22 -0400

    create resp. processing, plotting functions. create resp. processing, analyzing drake plans and markdown report.

--- code/2-respiration.R
+++ code/2-respiration.R
@@ -4,52 +4,202 @@ library(tidyverse)
 library(drake)
 library(PNWColors)
 
+# respiration functions -- compute ---------------------------------------------------
 
-respiration_plan = 
-  drake_plan(
-  # load file ---------------------------------------------------------------
-  resp_data = read.csv(file_in("data/respiration.csv")),
-  core_key = read.csv(file_in("data/core_key.csv")) %>% 
-    mutate(core = as.character(core)),
-  theme_set(theme_bw()),
+clean_lgr_output = function(resp_lgr){
+  resp_lgr %>% 
+    filter(!is.na(ID)) %>% 
+    rename(CO2_626_ppm = `X.CO2_626._ppm`,
+           CO2_636_ppm = `X.CO2_636._ppm`) %>% 
+    dplyr::select(Date, Time, ID, CO2_626_ppm, CO2_636_ppm, D13C_VPDB_CO2) %>% 
+    separate(ID, sep = " ", into = c("A", "B", "C")) %>% 
+    filter(!C %in% c("B", "b")) %>% 
+    dplyr::select(-B, -C) %>% 
+    separate(A, sep = "_", into = c("core", "B")) %>% 
+    dplyr::select(-B) %>% 
+    mutate(core = str_replace_all(core, ":",""))
+}
+calculate_RC13 = function(lgr_clean){
+  lgr_clean %>% 
+    group_by(core) %>% 
+    dplyr::summarise(CO2_626_ppm = mean(CO2_626_ppm),
+                     CO2_636_ppm = mean(CO2_636_ppm),
+                     D13C_VPDB_CO2 = mean(D13C_VPDB_CO2)) %>% 
+    ungroup() %>% 
+    mutate(RC13 = CO2_636_ppm/CO2_626_ppm,
+           D13C_calc = ((RC13/0.011237)-1)*1000) %>% 
+    dplyr::select(-CO2_626_ppm, -CO2_636_ppm, -D13C_VPDB_CO2)
+}
+
+clean_licor_output = function(resp_licor){
+  resp_licor_temp = 
+    resp_licor %>% 
+    rename(pCO2_ppm = `pCO2.raw..ppm.`) %>% 
+    dplyr::select(ID, pCO2_ppm) %>% 
+    separate(ID, sep = ":", into = c("core", "B")) %>% 
+    dplyr::select(-B)
+  
+  ambient = 
+    resp_licor_temp %>% 
+    mutate(ambient = grepl("A", core)) %>% 
+    filter(ambient) %>% 
+    filter(!core %in% "A1") %>% 
+    dplyr::summarise(pCO2_ppm = round(mean(pCO2_ppm),3)) %>% pull()
   
   
-  # clean file --------------------------------------------------------------
-  respiration = 
-    resp_data %>% 
-    filter(is.na(rep)|rep!="B") %>% 
-    separate(lgr_id, sep = ": ", into = c("core", "treatment")) %>% 
-    select(-treatment) %>% 
-    left_join(select(core_key, core, type, treatment)) %>% 
-    filter(!is.na(treatment)),
+  resp_licor_temp %>% 
+    mutate(pCO2_ppm = pCO2_ppm - ambient)
+    
+}
+calculate_moles_CO2C = function(headspace, licor_clean){
+  P = 1         # atm
+  R = 82.05			# cm3 atm K−1 mol−1
+  Temp = 21+273 # K 
+  
+  headspace_calc  = 
+    headspace %>% 
+    mutate(mmol_air = 1000*(P*headspace_vol_mL)/(R*Temp))
+  
+  licor_clean %>% 
+    left_join(dplyr::select(headspace_calc, core, mmol_air), by = "core") %>% 
+    mutate(mmol_CO2C = pCO2_ppm * mmol_air/1000000,
+           umol_CO2C = round(mmol_CO2C * 1000,3)) %>% 
+    dplyr::select(core, pCO2_ppm, umol_CO2C)
+  
+  #list(licor_clean2 = licor_clean2)
+}
+
+compute_priming = function(dat){
+  # formula taken from Bastida et al. Nat. Comms. https://doi.org/10.1038/s41467-019-11472-7 
+  
+  resp_mean = 
+    dat %>% 
+    group_by(type, treatment) %>% 
+    dplyr::summarise(umol_12CO2C = round(mean(umol_12CO2C),3),
+                     umol_13CO2C = round(mean(umol_13CO2C),3),
+                     umol_CO2C = round(mean(umol_CO2C),3)) %>% 
+    ungroup()
   
   
+  control_CO2C_umol = resp_mean %>% filter(type %in% "control") %>% pull(umol_CO2C)
+  substrate_CO2C_umol = resp_mean %>% filter(!type %in% "control") %>% pull(umol_CO2C)
+  substrate_13CO2C_umol = resp_mean %>% filter(!type %in% "control") %>% pull(umol_13CO2C)
   
-  # plots -------------------------------------------------------------------
+  priming = (substrate_CO2C_umol - substrate_13CO2C_umol) - control_CO2C_umol
   
-  gg_13c = 
+  tibble(priming_umolC = priming)
+}
+make_priming_table = function(combined_lgr_licor){
+  priming_desorption = 
+    combined_lgr_licor %>% 
+    filter(!type %in% "priming") %>% 
+    group_by(treatment) %>% 
+    do(compute_priming(.)) %>% 
+    rename(desorption_umolC = priming_umolC)
+  
+  priming_priming = 
+    combined_lgr_licor %>% 
+    filter(!type %in% "desorption") %>% 
+    group_by(treatment) %>% 
+    do(compute_priming(.)) %>% 
+    rename(priming_umolC = priming_umolC)
+  
+  priming_table = 
+    priming_desorption %>% left_join(priming_priming)
+  
+  list(priming_table = priming_table)
+}
+
+
+# respiration functions -- plotting ---------------------------------------
+
+plot_respiration = function(respiration){
+  gg_resp = 
     respiration %>% 
-    ggplot(aes(x = treatment, y = c13_co2_permil, color = type))+
-    geom_point(size=3, position = position_dodge(width = 0.5))+
-    scale_color_manual(values = pnw_palette("Sailboat", 3))+
-    NULL,
-  
-  gg_co2 = 
-    respiration %>% 
-    ggplot(aes(x = treatment, y = co2_ppm, color = type))+
+    ggplot(aes(x = treatment, y = pCO2_ppm, color = type))+
     geom_boxplot()+
     geom_point(size=3, position = position_dodge(width = 0.7))+
     scale_color_manual(values = pnw_palette("Sailboat", 3))+
-    NULL,
+    labs(title = "partial pressure of CO2",
+         caption = "blank-corrected with ambient")+
+    NULL
   
+  gg_D13C = 
+    respiration %>% 
+    ggplot(aes(x = treatment, y = D13C_calc, color = type))+
+    geom_point(size=3, position = position_dodge(width = 0.5))+
+    scale_color_manual(values = pnw_palette("Sailboat", 3))+
+    labs(title = "Δ13C-CO2")+
+    NULL
   
-  # report ------------------------------------------------------------------
+  gg_R13C = 
+    respiration %>% 
+    ggplot(aes(x = treatment, y = RC13, color = type))+
+    geom_point(size=3, position = position_dodge(width = 0.5))+
+    scale_color_manual(values = pnw_palette("Sailboat", 3))+
+    labs(title = "R C13/C12 in CO2")+
+    NULL 
   
-  report = rmarkdown::render(
-    knitr_in("reports/respiration_drake_report.Rmd"),
-    output_format = rmarkdown::github_document())  
+  gg_13C_umol = 
+    respiration %>% 
+    ggplot(aes(x = treatment, y = umol_13CO2C, color = type))+
+    geom_point(size=3, position = position_dodge(width = 0.5))+
+    scale_color_manual(values = pnw_palette("Sailboat", 3))+
+    labs(title = "umol of 13C-CO2")+
+    NULL
   
-  # -------------------------------------------------------------------------
+  list(gg_resp = gg_resp,
+       gg_D13C = gg_D13C,
+       gg_R13C = gg_R13C,
+       gg_13C_umol = gg_13C_umol)  
+}
+
+#  
+
+# respiration processing plan ---------------------------------------------------------------------
+respiration_processing_plan = 
+  drake_plan(
+    core_key = read.csv(file_in("data/core_key.csv")) %>% mutate(core = as.character(core)),
+    headspace = read.csv("data/respiration_headspace.csv"),
+    resp_lgr = read.csv("data/respiration_lgr_output.csv"),
+    resp_licor = read.csv("data/respiration_licor_output.csv"),
+    
+    lgr_clean = clean_lgr_output(resp_lgr),
+    lgr_calc = calculate_RC13(lgr_clean),
+    
+    licor_clean = clean_licor_output(resp_licor),
+    licor_moles = calculate_moles_CO2C(headspace, licor_clean),
+    
+    combined_lgr_licor = 
+      lgr_calc %>% 
+      left_join(licor_moles, by = "core") %>% 
+      mutate(umol_12CO2C = umol_CO2C/(RC13 + 1),
+             umol_13CO2C = umol_CO2C - umol_12CO2C) %>% 
+      left_join(select(core_key, core, type, treatment), by = "core") %>% 
+      filter(!is.na(treatment)),
+    
+    respiration_output = combined_lgr_licor %>% 
+      write.csv("data/processed/respiration.csv", row.names = F)
+  )
+
+make(respiration_processing_plan)
+
+
+
+# respiration analysis plan -----------------------------------------------
+
+respiration_analysis_plan = 
+  drake_plan(
+    theme_set(theme_bw()),
+    respiration = read.csv(file_in("data/processed/respiration.csv")),
+    priming_table = make_priming_table(respiration) %>% knitr::kable(),
+    gg_respiration = plot_respiration(respiration),
+    
+    # report
+    report = rmarkdown::render(
+      knitr_in("reports/respiration_drake_report.Rmd"),
+      output_format = rmarkdown::github_document())
 )
 
-make(respiration_plan)
+make(respiration_analysis_plan)
+
--- reports/respiration_drake_report.md
+++ reports/respiration_drake_report.md
@@ -1,6 +1,90 @@
-respiration
+DOM destabilization: respiration
 ================
 
-![](markdown/respiration/unnamed-chunk-1-1.png)<!-- -->
+two treatments:
 
-![](markdown/respiration/unnamed-chunk-2-1.png)<!-- -->
+1.  desorption: 13C oxalic acid adsorbed to goethite, 5 g goethite+OA
+    added to soil
+2.  priming: 13C oxalic acid added to soil as solution, 5 g goethite
+    (without substrate) added to soil
+3.  control: 5 g goethite added to soil, but no OA, only water
+
+Jars were sealed for 48 hours, after which headspace samples were
+collected and analyzed for CO2 concentration and 13C/12C composition of
+CO2.
+
+-----
+
+![](markdown/respiration/resp_plots-1.png)<!-- -->![](markdown/respiration/resp_plots-2.png)<!-- -->![](markdown/respiration/resp_plots-3.png)<!-- -->![](markdown/respiration/resp_plots-4.png)<!-- -->
+
+-----
+
+### priming
+
+How much priming was seen in the treatments?
+
+Priming calculated as: *(CO2C\_umol - 13CO2C\_umol) -
+control\_CO2C\_umol*
+
+[from Bastida et al. Nat.
+Comms.](https://doi.org/10.1038/s41467-019-11472-7)
+
+| treatment          | desorption\_umolC | priming\_umolC |
+| :----------------- | ----------------: | -------------: |
+| 1-time-zero        |           \-1.968 |        \-7.597 |
+| 2-wetting          |          \-12.481 |          1.029 |
+| 4-drying-rewetting |           \-6.167 |             NA |
+
+-----
+
+<details>
+
+<summary>Session Info</summary>
+
+Date Run: 2020-09-24
+
+    #> R version 4.0.2 (2020-06-22)
+    #> Platform: x86_64-apple-darwin17.0 (64-bit)
+    #> Running under: macOS Catalina 10.15.6
+    #> 
+    #> Matrix products: default
+    #> BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib
+    #> LAPACK: /Library/Frameworks/R.framework/Versions/4.0/Resources/lib/libRlapack.dylib
+    #> 
+    #> locale:
+    #> [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+    #> 
+    #> attached base packages:
+    #> [1] stats     graphics  grDevices utils     datasets 
+    #> [6] methods   base     
+    #> 
+    #> other attached packages:
+    #>  [1] PNWColors_0.1.0 drake_7.12.4    forcats_0.5.0  
+    #>  [4] stringr_1.4.0   dplyr_1.0.1     purrr_0.3.4    
+    #>  [7] readr_1.3.1     tidyr_1.1.1     tibble_3.0.3   
+    #> [10] ggplot2_3.3.2   tidyverse_1.3.0
+    #> 
+    #> loaded via a namespace (and not attached):
+    #>  [1] Rcpp_1.0.5        lubridate_1.7.9   txtq_0.2.3       
+    #>  [4] prettyunits_1.1.1 assertthat_0.2.1  digest_0.6.25    
+    #>  [7] packrat_0.5.0     R6_2.4.1          cellranger_1.1.0 
+    #> [10] backports_1.1.8   reprex_0.3.0      evaluate_0.14    
+    #> [13] httr_1.4.2        highr_0.8         pillar_1.4.6     
+    #> [16] rlang_0.4.7       progress_1.2.2    readxl_1.3.1     
+    #> [19] rstudioapi_0.11   blob_1.2.1        rmarkdown_2.3    
+    #> [22] labeling_0.3      igraph_1.2.5      munsell_0.5.0    
+    #> [25] broom_0.7.0       compiler_4.0.2    modelr_0.1.8     
+    #> [28] xfun_0.16         pkgconfig_2.0.3   htmltools_0.5.0  
+    #> [31] tidyselect_1.1.0  fansi_0.4.1       crayon_1.3.4     
+    #> [34] dbplyr_1.4.4      withr_2.2.0       grid_4.0.2       
+    #> [37] jsonlite_1.7.0    gtable_0.3.0      lifecycle_0.2.0  
+    #> [40] DBI_1.1.0         magrittr_1.5      storr_1.2.1      
+    #> [43] scales_1.1.1      cli_2.0.2         stringi_1.4.6    
+    #> [46] farver_2.0.3      fs_1.5.0          xml2_1.3.2       
+    #> [49] ellipsis_0.3.1    filelock_1.0.2    generics_0.0.2   
+    #> [52] vctrs_0.3.2       tools_4.0.2       glue_1.4.1       
+    #> [55] hms_0.5.3         parallel_4.0.2    yaml_2.2.1       
+    #> [58] colorspace_1.4-1  base64url_1.4     rvest_0.3.6      
+    #> [61] knitr_1.29        haven_2.3.1
+
+</details>
